f6d675230650733310d80f9a2c609df06c7befd2
VINICIUS BUFFON <vinicius.buffon@acad.univates>
2022-10-04 22:14:23 -0300

    Variaveis

--- intro/01saidas.php
+++ intro/01saidas.php
@@ -33,19 +33,41 @@ print "<h4>Olá: " . "{$usuario}</h4>";
  */
 fullStackPHPClassSession("print_r", __LINE__);
 
+$array = ["LAJES", "TEUTONIENSE", "BOLOTAA"];
+
+print_r($array, True);
+
+$vetor = print_r($array, true);
+
+echo "<pre>". $vetor . "</pre>";
+
+
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
  */
 fullStackPHPClassSession("printf", __LINE__);
 
+$article = "<article><h2>%s</h2><p>%s</p></article>";
+$titulo = "Título da Notícia";
+$noticia = "Texto da notícia";
+
+printf($article,$titulo,$noticia);
 
 /**
  * [ vprintf ] https://php.net/manual/pt_BR/function.vprintf.php
  */
 fullStackPHPClassSession("vprintf", __LINE__);
 
+$article2 = "<aricle><h2>INSTITUICAO: %2</h2><p>Curso: %s</p></article>";
+
+$array_company = ["UNIVATES", "TI"];
+
+vprintf($article2, $array_company);
+
 
 /**
  * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
  */
 fullStackPHPClassSession("var_dump", __LINE__);
+
+var_dump($array);
--- intro/02variaveis.php
+++ intro/02variaveis.php
@@ -8,20 +8,75 @@ fullStackPHPClassName("Variáveis e tipos de dados");
  */
 fullStackPHPClassSession("variáveis", __LINE__);
 
+$firstName = "Vinícius";
+$city = "Lajeado";
+$years =  20;
+
+echo ("<h1>Olá me chamo " . $firstName . " e moro em " . $city . " esse ano fiz " . $years . ".</h1>");
+
+$pre_feito = "<h1>Olá me chamo %s e moro em %s esse ano fiz %d.</h1>";
+printf($pre_feito, $firstName, $city, $years);
+
+var_dump([
+    "IDADE: " => $years
+]);
 
 /**
  * [ tipo boleano ] true | false
  */
 fullStackPHPClassSession("tipo boleano", __LINE__);
 
+$verdade = true;
+$falso = false;
+
+$idadePessoa = 20;
+$maiorIdade = ($idadePessoa >= 18);
+
+echo "MAIOR IDADE = " . ($maiorIdade ? "SIM" : "NÃO");
+
+var_dump([$verdade, $falso, $maiorIdade]);
 
 /**
  * [ tipo callback ] call | closure
  */
 fullStackPHPClassSession("tipo callback", __LINE__);
 
+$code = "<h3>Título H3</h3><h4>Título H4</h4>";
+$cleanCode = call_user_func("strip_tags", $code);
+echo $code;
+echo $cleanCode;
+
+var_dump([
+    'code' => $code,
+    'cleancode' => $cleanCode
+]);
+
+$retorno = function($parametro){
+  echo "Saída = " . $parametro; 
+};
+
+$retorno("Univates");
 
 /**
  * [ outros tipos ] string | array | objeto | numérico | null
  */
 fullStackPHPClassSession("outros tipos", __LINE__);
+
+$string = "Vini";
+$array = array("Olá","Mundo");
+$objeto = new stdClass();
+$objeto -> nome = $string;
+$objeto -> idade = $years;
+$objeto -> cidade = $city;
+
+$int = 10;
+$float = 127.50;
+$null = null;
+
+echo "<pre>";
+var_dump([
+    $string,
+    $array,
+    $objeto
+]);
+echo "</pre>";
