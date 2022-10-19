daf118be99d2ac889e2f7f4f886f52866634067e
vinibuffon-git <zagvinizag@gmail.com>
2022-10-18 22:15:51 -0300

    Strings e Arrays

--- intro/04strings.php
+++ intro/04strings.php
@@ -7,20 +7,72 @@ fullStackPHPClassName("Funções para strings");
  */
 fullStackPHPClassSession("strings e multibyte", __LINE__);
 
+$curso = "Técnico em informática";
+
+var_dump([
+    "string" => $curso,
+    "strlen" => strlen($curso),
+    "mb_strlen" => mb_strlen($curso),
+    "substr" => substr($curso, 11),
+    "mb_substr" => mb_substr($curso, 11),
+    "strtoupper" => strtoupper($curso),
+    "mb_strtoupper" => mb_strtoupper($curso),
+    "strtolower" => strtolower($curso),
+    "mb_strtolower" => mb_strtolower($curso),
+]);
+
+$stringarray = array ("Bolacha","Biscoito","Melão","Lapada","Frog");
+
+for ($i=0; $i <count($stringarray) ; $i++) { 
+    $numDeLetras = mb_strlen($stringarray[$i]);
+    echo "<h4>{$stringarray[$i]} = {$numDeLetras}</h4>";
+}
+
+for ($i=0; $i <count($stringarray) ; $i++) { 
+    $palavraMaiu = mb_strtoupper($stringarray[$i]);
+    echo "<h4>{$stringarray[$i]} = {$palavraMaiu}</h4>";
+}
 
 /**
  * [ conversão de caixa ] https://php.net/manual/en/function.mb-convert-case.php
  */
 fullStackPHPClassSession("conversão de caixa", __LINE__);
 
+var_dump([
+    "strtoupper" => strtoupper($curso),
+    "mb_strtoupper" => mb_strtoupper($curso),
+    "strtolower" => strtolower($curso),
+    "mb_strtolower" => mb_strtolower($curso),
+    "mb_convert_case UPPER" => mb_convert_case($curso, MB_CASE_UPPER),
+    "mb_convert_case LOWER" => mb_convert_case($curso, MB_CASE_LOWER),
+    "mb_convert_case TITLE" => mb_convert_case($curso, MB_CASE_TITLE)
+]);
 
 /**
  * [ substituição ] multibyte and replace
  */
 fullStackPHPClassSession("substituição", __LINE__);
 
+$substituicao = "programação para internet";
+
+echo "<h5>{$substituicao}<h5>";
+
+echo "<h5>" . str_replace("internet", "android", $substituicao) . "</h5>";
+
+echo "<h5>" . str_replace(["programação", "internet"], ["desenvolvimento", "android"], $substituicao) . "</h5>";
 
 /**
  * [ parse string ] parse_str | mb_parse_str
  */
 fullStackPHPClassSession("parse string", __LINE__);
+
+$site = "https://www.google.com.br/search?q=univates&source=hp&ei=PEBPY4_CKInM1sQPu_ukmAQ&iflsig=AJiK0e8AAAAAY09OTHPSTSx2z6xaHoD1LqcoykcExz5v&ved=0ahUKEwiP0LavgOv6AhUJppUCHbs9CUMQ4dUDCAg&uact=5&oq=univates&gs_lcp=Cgdnd3Mtd2l6EAMyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATIFCAAQgAQyBQgAEIAEMgsILhCABBDHARCvATIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgoILhDqAhC0AhBDOg0ILhDUAhDqAhC0AhBDOgoIABDqAhC0AhBDOhEILhCABBCxAxCDARDHARDRAzoOCC4QgAQQsQMQgwEQ1AI6CAguELEDEIMBOggILhCABBDUAjoFCC4QgAQ6DgguELEDEIMBEMcBENEDOggIABCABBCxAzoLCC4QgAQQsQMQgwFQuAZY6Axgxw5oCXAAeACAAQCIAQCSAQCYAQCgAQGwAQo&sclient=gws-wiz";
+
+mb_parse_str($site, $siteComParametros);
+
+var_dump([
+    "site" => $site,
+    "parametros" => $siteComParametros,
+]);
+
+
--- intro/05arrays.php
+++ intro/05arrays.php
@@ -7,11 +7,69 @@ fullStackPHPClassName("Funções para arrays");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$array = [
+    "Javascript",
+    "PHP",
+    "Java"
+];
+
+$assoc = [
+    "Lang2" => "Javascript",
+    "Lang1" => "PHP",
+    "Lang3" => "Java",
+];
+
+array_unshift($array, "C#", "C++"); //adicionando novos itens na frente do vetor
+
+array_push($array, "Assembly"); //adiciona um novo elemento no final do vetor
+
+var_dump($array, $assoc);
+
+$novo_array = ["Ruby", "React"] + $array;
+
+$novo_assoc = ["Lang4" => "C#"] + $assoc;
+
+var_dump($novo_array, $novo_assoc);
 
 /*
  * [ ordenação ] reverse | asort | ksort | sort
  */
 fullStackPHPClassSession("ordenação", __LINE__);
+
+$novo_assoc_ksort = $novo_assoc;
+
+//asort($novo_assoc); //ordenação através do valor do item e mantem a chave ao invés de retornar apenas o índice
+
+echo "<h6>ordenação através do valor do item</h6>";
+sort($novo_assoc); //ordenação através do valor do item
+var_dump($novo_assoc);
+
+echo "<h6>ordenação através da chave do índice - array associativo</h6>";
+ksort($novo_assoc_ksort); //ordenação através da chave do índice - array associativo
+var_dump($novo_assoc_ksort);
+
+
+$reverse_array = [
+    "A","B","F","Z","X"
+];
+
+echo "<h6>ordenação reversa do valor do item</h6>";
+$novo_reverse_array = array_reverse($reverse_array, true);
+var_dump($novo_reverse_array);
+
+/*
+    1 - Segue o ARRAY:
+        ["A", "B", "X", "Z", "F", "E", "J", "Ç"]
+        -> Imprima este array de forma alfabética
+        -> Imprima este array de forma alfabética ao contrário 
+*/
+
+$alfabeto = array ("A", "B", "X", "Z", "F", "E", "J", "Ç");
+
+sort($alfabeto);
+var_dump($alfabeto);
+$reverse_alfabeto = array_reverse($alfabeto, true);
+var_dump($reverse_alfabeto);
 
 
 /*
@@ -19,6 +77,10 @@ fullStackPHPClassSession("ordenação", __LINE__);
  */
 fullStackPHPClassSession("verificação", __LINE__);
 
+echo "<h5>Verificar se existe PHP dentro do array</h5>";
+if(in_array("PHP", $array)){
+    echo "TEM PHP!";
+}
 
 /**
  * [ exemplo prático ] um template view | implode
